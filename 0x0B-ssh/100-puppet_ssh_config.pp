0d30b53c9b104e32f8241821e756441b112ebacb
koredeycode <koredey4u@gmail.com>
2023-01-16 21:00:50 +0000

    this

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,15 +2,15 @@
 include stdlib
 
 file_line { 'No authentication':
-    ensure => present,
-    path => '/etc/ssh/ssh_config',
-    line => '  PasswordAuthentication no',
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    line    => '  PasswordAuthentication no',
     replace => true,
 }
 
 file_line { 'identity file':
-    ensure => present,
-    path => '/etc/ssh/ssh_config',
-    line => '  IdentityFile ~/.ssh/school',
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    line    => '  IdentityFile ~/.ssh/school',
     replace => true,
 }
