ab9ee9054816167277b84e6e626b409f6c8a7e60
koredeycode <koredey4u@gmail.com>
2023-01-19 19:27:02 +0000

    this

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,6 @@
-#installs a nginx server
+# Installs a Nginx server
 
-exec {'install_nginx':
-    command => 'sudo apt-get -y update; sudo apt-get -y install nginx',
-    unless  => '/usr/bin/dpkg -l | grep nginx',
-}
-
-exec {'index_html':
-    command => "echo 'Hello World!' > /var/www/html/index.html",
-}
-
-exec {'redirect_config':
-    command => "sudo sed -i 's/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=dQw4w9WgXcQ permanent;/' /etc/nginx/sites-available/default"
-}
-
-exec {'reload_nginx':
-    command => 'sudo service nginx start',
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/koredeycode permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
