09eb2f1133f89de0425562345ec4826c2d3116fb
koredeycode <koredey4u@gmail.com>
2023-02-08 22:29:56 +0000

    this

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
 """Gather data from a API"""
 
-import csv
 import requests
 import sys
 
 
 def main():
-    url1 = 'https://jsonplaceholder.typicode.com/users'
-    url2 = 'https://jsonplaceholder.typicode.com/todos'
-    userId = int(sys.argv[1])
+    url = 'https://jsonplaceholder.typicode.com/'
+    userId = sys.argv[1]
 
-    with requests.get(url1) as response:
-        users = response.json()
-        for user in users:
-            if user.get("id") == userId:
-                employee = user.get("name")
-                break
+    with requests.get(url + "users/{}".format(userId)) as response:
+        user = response.json()
 
-    with requests.get(url2) as response:
+    with requests.get(url + "todos", params={"userId": userId}) as response:
         toDos = response.json()
-        userToDos = [toDo for toDo in toDos if toDo.get("userId") == userId]
+        total = len(toDos)
+        toDosDone = [toDo.get("title") for toDo in toDos
+                     if toDo.get("completed") is True]
+        done = len(toDosDone)
 
-    with open("{}.csv".format(userId), "w") as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for userTodo in userToDos:
-            completed = userTodo.get("completed")
-            title = userTodo.get("title")
-            writer.writerow([userId, employee, completed, title])
+        tstr = "\n\t ".join(toDosDone)
+        out = "Employee {} is done with tasks({}/{}):\n\t {}".\
+              format(user.get("name"), done, total, tstr)
+        print(out)
 
 
 if __name__ == "__main__":
