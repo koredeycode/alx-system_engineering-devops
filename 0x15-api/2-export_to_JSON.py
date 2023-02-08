66c0e734bae5edb3751c5d7ca48ecd0861fdc677
koredeycode <koredey4u@gmail.com>
2023-02-08 23:03:52 +0000

    two

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,30 +7,23 @@ import sys
 
 
 def main():
-    url1 = 'https://jsonplaceholder.typicode.com/users'
-    url2 = 'https://jsonplaceholder.typicode.com/todos'
-    userId = int(sys.argv[1])
+    url = 'https://jsonplaceholder.typicode.com/'
+    userId = sys.argv[1]
 
-    with requests.get(url1) as response:
-        users = response.json()
-        for user in users:
-            if user.get("id") == userId:
-                employee = user.get("name")
-                break
+    with requests.get(url + "users/{}".format(userId)) as response:
+        user = response.json()
 
-    with requests.get(url2) as response:
+    with requests.get(url + "todos", params={"userId": userId}) as response:
         toDos = response.json()
-        userToDos = [toDo for toDo in toDos if toDo.get("userId") == userId]
-        userId = str(userId)
         objs = {userId: []}
-        for userToDo in userToDos:
+        name = user.get("name")
+        for toDo in toDos:
             obj = {
-                    "task": userToDo.get("title"),
-                    "completed": userToDo.get("completed"),
-                    "username": employee
+                    "task": toDo.get("title"),
+                    "completed": toDo.get("completed"),
+                    "username": name
                     }
             objs.get(userId).append(obj)
-
         with open(userId + ".json", "w") as f:
             json.dump(objs, f)
 
